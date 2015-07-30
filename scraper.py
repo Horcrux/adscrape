f06079490fa808328112071b6ec3e6971770ecf6
Dawid Botha <pizzabotha@gmail.com>
2015-07-30 14:25:05 +0200

    scrape telephone numbers from ads

--- scraper.py
+++ scraper.py
@@ -7,22 +7,31 @@ def getadlist(url):
     r  = requests.get(url);
     data = r.text
     soup = BeautifulSoup(data, "lxml")
+    print ("this is called")
     ads = []
     for link in soup.find_all('a'):
         if (link.get('href').startswith("/a-")):
             ads.append("http://www.gumtree.co.za"+link.get('href'))
     return ads
 
-#Return a list of lists of ads' contents
+#Return a list of ads' contents
 def scrapead(url):
     r  = requests.get(url);
     data = r.text
     soup = BeautifulSoup(data, "lxml")
-    mydivs = soup.findAll("span", { "class" : "pre" })
-    print (type (mydivs))
-    print (mydivs)
+    adinfo = []
+    adinfo.append('adtitle')
+    telephonesoup=soup.findAll('a',{'class':'button telephone'})
+    if telephonesoup!=[]:
+        adinfo.append(telephonesoup[0]['href'])
+    else:
+        adinfo.append("none")
+    return adinfo
 
+adsinfo = []
 for ad in getadlist(sys.argv[1]):
-    print("ad="+ad)
-    scrapead(ad)
+    print ad
+    adsinfo.append(scrapead(ad))
     print("************************************************")
+
+print adsinfo
