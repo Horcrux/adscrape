c99f6e6aebc8bc74cf8313ef50255bb148ecf4e4
Dawid Botha <pizzabotha@gmail.com>
2015-08-12 11:15:48 +0200

    strip urls before passing to module

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import sys
 print "***************************STARTUP***************************"
 with open ('scrapelist.cfg', 'r') as f:
     for line in f:
+        print line.split(' ')[2].strip().split('/')[2]
 	print ("Now handling line \""+line.strip()+"\"")
-        if (line.split(' ')[3].strip()=='gumtree'):
-            gumtree.main(line.split(' ')[2],line.split(' ')[1],int(line.split(' ')[0]))
-        elif (line.split(' ')[3].strip()=='olx'):
-            olx.main(line.split(' ')[2],line.split(' ')[1],int(line.split(' ')[0]))
+        if (line.split(' ')[2].strip().split('/')[2].split('.')[1]=='gumtree'):
+            gumtree.main(line.split(' ')[2].strip(),line.split(' ')[1],int(line.split(' ')[0]))
+        elif (line.split(' ')[2].strip().split('/')[2].split('.')[1]=='olx'):
+            olx.main(line.split(' ')[2].strip(),line.split(' ')[1],int(line.split(' ')[0]))
